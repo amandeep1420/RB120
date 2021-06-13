5056f6ab7d8c399e88924bf3884da00d697f42e1
amandeep1420 <amandeep1420@gmail.com>
2021-06-13 23:02:08 +0000

    Added Debugging exercises.

---
+++ Exercises/Debugging/7_bank.rb
@@ -0,0 +1,56 @@
+class BankAccount
+  attr_reader :balance
+
+  def initialize(account_number, client)
+    @account_number = account_number
+    @client = client
+    @balance = 'meow'
+  end
+
+  def deposit(amount)
+    if amount > 0
+      self.balance += amount
+      "$#{amount} deposited. Total balance is $#{balance}."
+    else
+      "Invalid. Enter a positive amount."
+    end
+  end
+
+  def withdraw(amount)
+    withdrawal = self.balance - amount
+    
+    if valid_transaction?(withdrawal) && amount > 0 #forgot to re-check amount > 0
+      success = true
+    else
+      success = false
+    end
+
+    if success
+      "$#{amount} withdrawn. Total balance is $#{balance}."
+    else
+      "Invalid. Enter positive amount less than or equal to current balance ($#{balance})."
+    end
+  end
+
+  def balance=(new_balance)
+    @balance = new_balance
+    new_balance.upcase!
+  end
+
+  def valid_transaction?(new_balance)
+    new_balance >= 0
+  end
+end
+
+# Example
+
+account = BankAccount.new('5538898', 'Genevieve')
+
+p account.balance = 'cat'
+
+#                           # Expected output:
+# p account.balance         # => 0
+# p account.deposit(50)     # => $50 deposited. Total balance is $50.
+# p account.balance         # => 50
+# p account.withdraw(80)    # => Invalid. Enter positive amount less than or equal to current balance ($50).
+# p account.balance         # => 50
