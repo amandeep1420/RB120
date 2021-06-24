12ea145b2ff652cb3e8b20b3878de173db24c12a
amandeep1420 <amandeep1420@gmail.com>
2021-06-24 17:28:22 +0000

    Scratch.

--- scratch.rb
+++ scratch.rb
@@ -1,18 +1,1 @@
-class Person
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-  
-  def change_name
-    p name
-    name = 1
-    p name
-  end
-end
-
-bob = Person.new('Bob')
-p bob.name 
-bob.change_name
-p bob.name
+# nil
