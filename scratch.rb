53b7d59532132c9acfc9a8343d60922b9afcdd49
amandeep1420 <amandeep1420@gmail.com>
2021-07-02 02:51:05 +0000

    Interview prep.

--- scratch.rb
+++ scratch.rb
@@ -1,1 +1,29 @@
-# nil
+class Human
+  def initialize
+    @diet = "food"
+    @limbs = 4
+    @needs = ['air', 'water', 'shelter']
+  end
+end
+
+class Hand
+  def initialize
+    @cards = []
+  end
+  
+  def cards
+    @cards
+  end
+end
+
+class Card; end
+  
+hand = Hand.new
+
+p hand.cards
+
+hand.cards << Card.new
+hand.cards << Card.new
+hand.cards << Card.new
+
+p hand.cards
